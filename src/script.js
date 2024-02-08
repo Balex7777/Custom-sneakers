1c8a439dca7abaafe16693022dbe6505ab75eda2
Balex7777 <morevbalex777@gmail.com>
2024-02-08 14:08:49 +0300

    Init

---
+++ src/script.js
@@ -0,0 +1,54 @@
+const anchors = document.querySelectorAll('a[href*="#"]')
+const links = document.querySelectorAll('.head-links > a')
+const navigation = document.querySelector('.nav')
+const header = document.querySelector('header')
+let isNavOpen = false
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'center'
+    })
+  })
+}
+
+navigation.addEventListener("click", function(){
+  if (!isNavOpen){
+    navigation.classList.add("close-btn")
+    header.style.height = '100vh'
+  }else{
+    navigation.classList.remove("close-btn")
+    header.style.height = '100px'
+  }
+  isNavOpen = !isNavOpen
+})
+
+for (let link of links) {
+  link.addEventListener('click', function (e) {
+    navigation.classList.remove("close-btn")
+    header.style.height = '100px'
+    isNavOpen = false
+  })
+}
+
+function onEntry(entry) {
+  entry.forEach(change => {
+    if (change.isIntersecting) {
+     change.target.classList.add('element-show');
+    }
+  });
+}
+
+let options = {
+  threshold: [0.5] };
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.element-animation');
+
+for (let elm of elements) {
+  observer.observe(elm);
+}
